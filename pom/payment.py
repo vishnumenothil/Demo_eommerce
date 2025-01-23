78e3c6894458207f86c77b1f70187a9232b21ed7
vishnu <vishnumenothil@gmail.com>
2025-01-23 13:59:16 +0530

    payment2

--- pom/payment.py
+++ pom/payment.py
@@ -41,41 +41,12 @@ class Payment:
             s.enter_text(self._locator['txt_pin'],value=pin_code)
             s.enter_text(self._locator['txt_ph_no'],value=phone)
             self.except_bl(fname,lname,address,phone,cvv,card_no,city,pin_code,country,credit_cart,exp_mon,exp_year)
-            # s.click_element(self._locator['btn_continue1'])
-            # s.click_element(self._locator['btn_continue1'])
-            # s.click_element(self._locator['btn_continue2'])
-            # s.click_element(self._locator['btn_continue3'])
-            # s.click_element(self._locator['rad_cred'])
-            # s.click_element(self._locator['btn_continue4'])
-
-            # s.select_item(self._locator['selct_cardtype'],item=credit_cart)
-            # s.enter_text(self._locator['txt_cardname'],value=fname)
-            # s.enter_text(self._locator['txt_cardno'],value=card_no)
-            # s.select_item(self._locator['sele_exp_mon'],item=exp_mon)
-            # s.select_item(self._locator['sele_exp_year'],item=exp_year)
-            # s.enter_text(self._locator['txt_code'],value=cvv)
             
-            # s.click_element(self._locator['btn_continue5'])
-            # s.click_element(self._locator['btn_confirm'])
             
         except:
             self.except_bl(fname,lname,address,phone,cvv,card_no,city,pin_code,country,credit_cart,exp_mon,exp_year)
             
-            # s.click_element(self._locator['btn_continue1'])
-            # s.click_element(self._locator['btn_continue2'])
-            # s.click_element(self._locator['btn_continue3'])
-            # s.click_element(self._locator['rad_cred'])
-            # s.click_element(self._locator['btn_continue4'])
 
-            # s.select_item(self._locator['selct_cardtype'],item=credit_cart)
-            # s.enter_text(self._locator['txt_cardname'],value=fname)
-            # s.enter_text(self._locator['txt_cardno'],value=card_no)
-            # s.select_item(self._locator['sele_exp_mon'],item=exp_mon)
-            # s.select_item(self._locator['sele_exp_year'],item=exp_year)
-            # s.enter_text(self._locator['txt_code'],value=cvv)
-            
-            # s.click_element(self._locator['btn_continue5'])
-            # s.click_element(self._locator['btn_confirm'])
             
             
             
